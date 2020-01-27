210a2d8fe6504c5b35f72e20be5bf36b8f50f494
Abdul <abdul@uitoux.in>
2020-01-27 11:42:37 +0530

    u

--- src/CoaPkgServiceProvider.php
+++ src/CoaPkgServiceProvider.php
@@ -17,6 +17,8 @@ class CoaPkgServiceProvider extends ServiceProvider {
 		$this->loadViewsFrom(__DIR__ . '/views', 'coa-pkg');
 		$this->publishes([
 			__DIR__ . '/public' => base_path('public'),
+			__DIR__ . '/database/seeds/client' => 'database/seeds',
+			__DIR__ . '/config/config.php' => config_path('coa-pkg.php'),
 		]);
 	}
 
